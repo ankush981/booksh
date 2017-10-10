b123b217c2f6a5bad40ae4081a13e22c32c15e8a
Ankush <ankush.thakur53@gmail.com>
2017-10-11 00:27:57 +0530

    Add VueAxios to project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+
+import { routes } from './routes';
+import store from './store/index';
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
+
 new Vue({
   el: '#app',
+  store: store,
+  router: router,
   render: h => h(App)
 })
