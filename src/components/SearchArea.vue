95c148302cd63227e537fa219898e966b0042988
Ankush <ankush.thakur53@gmail.com>
2017-10-11 10:12:42 +0530

    Add SearchArea component

---
+++ src/components/SearchArea.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <input type="text" v-model='searchTerm' placeholder="Type search here"> 
+        <button @click="newSearch">Search</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                searchTerm: ''
+            }
+        },
+        methods: {
+            newSearch() {
+                console.log('I got clicked!');
+            }
+        }
+    }
+</script>
