845bc7745d08e7a6975c1307cd831f00e0b76828
Ankush <ankush.thakur53@gmail.com>
2017-10-12 17:37:00 +0530

    Change BrowseBooks to work with computed property automatically

--- src/components/BrowseBooks.vue
+++ src/components/BrowseBooks.vue
@@ -1,9 +1,24 @@
 <template>
-    <h1>Browse Books</h1>
+    <div>
+        <h3 v-if="!booksAvailable">No books found</h3>
+    </div>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                
+            }
+        },
+        computed: {            
+            books: function() {
+                return this.$store.getters.getBooksForCurrentPage;
+            },
+
+            booksAvailable: function() {
+                return this.books.length != 0;
+            }
+        }
     }
 </script>
