7db2ab54726ac233f3fa1acd8706e8f445b0e721
Ankush <ankush.thakur53@gmail.com>
2017-10-15 21:35:29 +0530

    Enhance SingleBook component

--- src/components/SingleBook.vue
+++ src/components/SingleBook.vue
@@ -1,9 +1,48 @@
 <template>
-    
+    <div class="book-listing">
+        <div class="book-image">
+            <img :src="book.volumeInfo.imageLinks.smallThumbnail">
+        </div>
+        
+        <div class="book-title">
+            {{ book.volumeInfo.title }}
+        </div>
+        
+        <div class="book-description">
+            {{ book.searchInfo.textSnippet }}
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        
+        props: [ 'book' ]
     }
 </script>
+
+<style scoped>
+    .book-image {
+        padding: 10px;
+    }
+
+    .book-title {
+        font-size: 1.2em;
+        font-weight: bold;
+        padding: 10px 0px;
+    }
+
+    .book-description {
+        padding: 20px 0px;
+    }
+
+    .book-listing {
+        margin: 0px auto;
+        margin-bottom: 20px;
+        max-width: 600px;
+    }
+
+    .book-listing img {
+        display: block;
+        margin: 10px auto;
+    }
+</style>
