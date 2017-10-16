949b7672d399c87b49c14fdb1b435e5c2d50b9ed
Ankush <ankush.thakur53@gmail.com>
2017-10-16 09:51:31 +0530

    Add logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <div id="header">
+            <img src="http://www.freeiconspng.com/uploads/-icons-smart-objects-3d-book-mockup-psd-address-book-icon-psd-preview-5.png" height="100">
             <div class="heading">Booksh!</div>
             <div class="sub-heading"><em>Un-crawl</em> the bookworm inside</div>
         </div>
