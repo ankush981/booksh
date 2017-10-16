22e8e0e3244f4dce7e2f1c79af8e38a600d460f0
Ankush <ankush.thakur53@gmail.com>
2017-10-16 09:21:41 +0530

    Style search area

--- src/components/SearchArea.vue
+++ src/components/SearchArea.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
-        <input type="text" v-model='searchTerm' placeholder="Type search here"> 
-        <button @click="newSearch">Search</button>
-        <div v-if="showSearchTerm">Showing results for "{{ searchTerm }}"</div>
+        <div id="search-area">
+            <input type="text" v-model='searchTerm' class="search-box"> 
+            <button @click="newSearch" class="search-button">Search</button>
+            <div v-if="showSearchTerm">Showing results for "{{ searchTerm }}"</div>
+        </div>
     </div>
 </template>
 
@@ -14,6 +16,7 @@
                 showSearchTerm: false
             }
         },
+
         methods: {
             newSearch() {
                 if(!this.searchTerm) {
@@ -25,3 +28,29 @@
         }
     }
 </script>
+
+<style>
+    #search-area {
+        text-align: center;
+        margin-top: 40px;    
+    }
+
+    .search-box {
+        display: inline-block;
+        font-size: 2em;
+        font-family: Merriweather;
+        color: #555;
+        border: 1px solid #BBB;
+        height: 40px;
+    }
+
+    .search-button {
+        display: inline-block;
+        font-size: 1.3em;
+        font-family: Merriweather;
+        color: #555;
+        padding: 6px;
+        position: relative;
+        top: -4px;
+    }
+</style>
