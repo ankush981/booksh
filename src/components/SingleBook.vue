38fde27e425590a89d42353de2ab63888db5efae
Ankush <ankush.thakur53@gmail.com>
2017-10-16 10:24:43 +0530

    Add border to SingleBook

--- src/components/SingleBook.vue
+++ src/components/SingleBook.vue
@@ -1,15 +1,20 @@
 <template>
     <div class="book-listing">
+        <div class="book-start-marker"></div>
+
+        <div class="book-title">
+            {{ book.volumeInfo.title }}
+        </div>
+
         <div class="book-image">
             <img :src="book.volumeInfo.imageLinks.smallThumbnail">
         </div>
         
-        <div class="book-title">
-            {{ book.volumeInfo.title }}
-        </div>
         
         <div class="book-description" v-html="book.searchInfo.textSnippet">
         </div>
+
+        <div class="book-end-marker"></div>
     </div>
 </template>
 
@@ -35,13 +40,18 @@
     }
 
     .book-listing {
+        padding: 20px;
+        padding-top: 40px;
         margin: 0px auto;
         margin-bottom: 20px;
         max-width: 600px;
+        border: 2px solid #DDD;
     }
 
     .book-listing img {
         display: block;
         margin: 10px auto;
     }
-</style>
+
+
+ </style>
