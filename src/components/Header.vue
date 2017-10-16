4c26972d10b96a39fed2d72f1789c3e06960c4d4
Ankush <ankush.thakur53@gmail.com>
2017-10-16 09:21:26 +0530

    Add Site title and sub-title to Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,10 @@
 <template>
-    
+    <div>
+        <div id="header">
+            <div class="heading">Booksh!</div>
+            <div class="sub-heading"><em>Un-crawl</em> the bookworm inside</div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -7,3 +12,19 @@
         
     }
 </script>
+
+<style>
+    #header {
+        text-align: center;
+        margin-top: 20px;
+    }
+
+    #header .heading {
+        font-size: 3em;
+    }
+
+    #header .sub-heading {
+        font-size: 1.4em;
+    }
+
+</style>
