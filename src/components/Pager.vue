3c9ff007109e7ef623a32fd16e084562eee97325
Ankush <ankush.thakur53@gmail.com>
2017-10-17 13:18:08 +0530

    Add Pager.vuw

---
+++ src/components/Pager.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div class="pager">
+            <span class="navigate" @click="pageBack"> <&nbsp; </span> 
+            <span class="page-num"> Page {{ currentPage }} </span> &nbsp;
+            <span class="navigate" @click="pageForward"> > </span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            currentPage() {
+                return this.$store.getters.getCurrentPage;
+            }
+        },
+
+        methods: {
+            pageBack() {
+                this.$store.dispatch('gotoPreviousPage');
+            },
+
+            pageForward() {
+                this.$store.dispatch('gotoNextPage');
+            }
+        }
+    }
+</script>
+
+<style>
+    .pager {
+        text-align: center;
+        font-size: 1.1em;        
+    }
+
+    .navigate {
+        display: inline-block;
+        font-size: 3em;
+        cursor: pointer;
+    }
+
+    .page-num {
+        position: relative;
+        top: -10px;
+    }
+</style>
